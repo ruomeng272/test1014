75fffb4e0042e1f9ba9b95929a161445cfe2fca0
ruomeng550 <1664077883@qq.com>
2018-10-14 12:29:13 +0800

    新增内容

--- src/components/List/list.vue
+++ src/components/List/list.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="hello">
       <h1>list页</h1>
+      <div>
+          <ul>
+              <li><p>我的代办</p><p>8个任务</p></li>
+              <li><p>本周任务平均处理时间</p><p>32分钟</p></li>
+              <li><p>本周完成任务数</p><p>24个任务</p></li>
+          </ul>
+      </div>
     </div>
   </template>
   
