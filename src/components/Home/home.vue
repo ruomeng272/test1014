c78bd3eab0a114b142e5fad13ce3985842db13ad
ruomeng550 <1664077883@qq.com>
2018-10-14 14:58:30 +0800

    新增home内容

--- src/components/Home/home.vue
+++ src/components/Home/home.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="hello">
       <h1>home页</h1>
+      <h4>活动实时交易情况</h4>
+      <div>
+          <ul>
+              <li><p>今日交易总额</p><p>123443123元</p></li>
+              <li><p>销售目标完成率</p><p>93%</p></li>
+              <li><p>每秒交易总额</p><p>234元</p></li>
+          </ul>
+      </div>
     </div>
   </template>
   
